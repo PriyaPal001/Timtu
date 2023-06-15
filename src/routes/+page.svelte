dedd5cdf80a7f48385baf4b7fd6b41aefa4a8e5a
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-06-15 21:30:11 +0530

    added layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
-  <style lang="postcss">
-    :global(html) {
-      background-color: pink;
-    }
-  </style>
+<section class="text-gray-600 body-font ">
+    <div class="container mx-auto flex border-4 border-red-500 px-24 py- md:flex-row  flex-col  items-center ">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+          <br class="hidden lg:inline-block">readymade gluten
+        </h1>
+        <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+        <div class="flex justify-center">
+          <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get started</button>
+          <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">learn more</button>
+          
+        </div>
+      </div>
+      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 border-4 mx-10 my-10 border-red-500">
+        <img class="object-cover object-center rounded h-full" alt="hero" src="https://img.freepik.com/free-vector/design-structure-matrix-abstract-concept-vector-illustration-project-visual-representation-system-analysis-project-management-organization-team-product-component-timeframe-abstract-metaphor_335657-2943.jpg?size=626&ext=jpg&uid=R87870808&ga=GA1.2.1007551031.1668626592&semt=sph">
+      </div>
+    </div>
+  </section>
+
+
+ 
